ec06598578e9972ff1993e990f77d3c62273f1ab
Camilo Paez <camilopaez.dev@gmail.com>
2024-05-16 18:06:43 -0500

    remove unused types

--- src/app/play/utils/types.d.ts
+++ src/app/play/utils/types.d.ts
@@ -1,26 +1,6 @@
-export interface Input {
-  name: string;
-}
-
-export interface Question {
-  description: string | string[];
-  input: Input | null;
-  interactions: Interaction[];
-  previus: Question | null;
-  variables?: { [key: string]: boolean };
-  prevValues?: PrevValues;
-}
-
 export interface PrevValues {
   whyBlood: string[] | string;
   whyKnife: string[] | string;
   whatYouDone: string[] | string;
   name: string;
-}
-
-export interface Interaction {
-  description: string;
-  dependencies?: string[];
-  type: 'GOTO' | 'TALK' | 'SIMPLE';
-  action: () => Question;
 }
