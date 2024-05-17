799064869be91c1cc8d3cf155a22067ff9b2a0a2
Camilo Paez <camilopaez.dev@gmail.com>
2024-05-16 22:47:49 -0500

    animated text file

---
+++ src/components/AnimatedText.tsx
@@ -0,0 +1,36 @@
+import { motion } from 'framer-motion';
+
+const defaultAnimations = {
+  hidden: {
+    opacity: 0,
+    y: 5,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
+const AnimatedText = ({ text }: { text: string[] }) => {
+  const arrayText = Array.isArray(text) ? text : [text];
+
+  return (
+    <>
+      <motion.span
+        initial="hidden"
+        animate="visible"
+        variants={defaultAnimations}
+        transition={{ duration: 0.5 }}
+      >
+        {arrayText.map((line) => (
+          <motion.h2 variants={defaultAnimations} key={line}>
+            - {line}
+            <div className="h-2"> </div>
+          </motion.h2>
+        ))}
+      </motion.span>
+    </>
+  );
+};
+
+export default AnimatedText;
